abea5094774471fa7f3b6186c513b666b8946701
Lhwhatever <llinhong.2301+github@gmail.com>
2022-10-16 14:09:54 -0700

    Delete some dead code and dead assets

--- src/components/AlarmListContext.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { AlarmReducerAction, Alarms } from "../store/alarms";
-
-const AlarmListContext = React.createContext<
-  [Alarms, React.Dispatch<AlarmReducerAction>]
->([{}, () => {}]);
-
-export default AlarmListContext;
